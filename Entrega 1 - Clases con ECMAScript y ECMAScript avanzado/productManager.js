9ede8dbf1cf214b262070a237a13233070b14085
Hisam Abou Faker <haboufaker@gmail.com>
2023-04-15 17:11:29 -0300

    First Commit - Desafio entregable 1

--- Entrega 1 - Clases con ECMAScript y ECMAScript avanzado/productManager.js
+++ Entrega 1 - Clases con ECMAScript y ECMAScript avanzado/productManager.js
@@ -18,14 +18,15 @@ class ProductManager {
             product.id = this.#id;
             this.products.push(product);
         } else {
-            this.products.forEach(existingProduct => {
-                if (existingProduct.code === product.code) {
-                    console.log("Error: The product code is already in the database; if you are trying to add a new product, please choose a different code.");
-                } else {
-                    product.id = this.#getId();
-                    this.products.push(product);
-                }
-            });
+            const existingProductCode = this.products.findIndex(existingProduct => existingProduct.code === product.code);
+
+            if (existingProductCode > -1) {
+                console.log("\nError: The product code is already in the database; if you are trying to add a new product, please choose a different code.\n");
+                return;
+            } else {
+                product.id = this.#getId();
+                this.products.push(product);
+            }
         };
     };
 
@@ -43,11 +44,11 @@ class ProductManager {
         const existingProductId = this.products.findIndex(existingProduct => existingProduct.id === id);
 
         if (existingProductId === -1) {
-            console.log("Error: Not found");
+            console.log("\nError: Not found\n");
             return;
         };
 
-        console.log("Product found");
+        console.log("\nProduct found\n");
         const product = this.products[id];
         console.log(product);
     };
@@ -61,7 +62,14 @@ productManager.addProduct("producto prueba", "Este es un producto prueba", 200, 
 productManager.getProductById(0);
 productManager.getProductById(1);
 productManager.addProduct("producto prueba 2", "Este es un producto prueba 2", 100, "Sin imagen 2", "abc124", 75);
+productManager.addProduct("producto prueba 3", "Este es un producto prueba 3", 600, "Sin imagen 3", "abc125", 15);
+productManager.addProduct("producto prueba 4", "Este es un producto prueba 4", 300, "Sin imagen 4", "abc126", 25);
+productManager.addProduct("producto prueba 5", "Este es un producto prueba 5", 400, "Sin imagen 5", "abc127", 85);
+productManager.addProduct("producto prueba 5", "Este es un producto prueba 5", 400, "Sin imagen 5", "abc127", 85);
 productManager.getProducts();
 productManager.getProductById(0);
 productManager.getProductById(1);
 productManager.getProductById(2);
+productManager.getProductById(8);
+productManager.getProductById(10);
+productManager.getProductById(12);
